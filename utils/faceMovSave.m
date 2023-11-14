029f6b2df75c04f10a5582c4b1e3904fe92f7cd2
Ruix Li <32361095+RuixLi@users.noreply.github.com>
2023-11-14 09:45:01 +0900

    revised

--- utils/faceMovSave.m
+++ utils/faceMovSave.m
@@ -3,7 +3,7 @@ save(fullfile(ops.saveFolder,[ops.saveName '.mat']),'-struct','ops','-v7.3')
 
 
 if isfield(ops, 'movieData')
-    % save the movieData into the save folder as an avi file
+    % save the movieData into the save folder as an mp4 file
     v = VideoWriter(fullfile(ops.saveFolder, 'movie_fmd'),"MPEG-4");
         open(v)
         im = newaxis(ops.movieData,3);
